2ea3ff0db02732fa857ab74eb0db318803c619a5
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-10-31 13:02:22 +0300

    Add files via upload

---
+++ örnek12.cpp
@@ -0,0 +1,17 @@
+//Rümeysa Coþkun
+//0'dan 100'e kadar olan sayýlarýn karesini alan program.
+#include<iostream>
+using namespace std;
+int main()
+{
+int i=0;
+int k;
+for(i;i<=100;i++)
+{
+k=i*i;	
+cout<<k<<endl;
+}
+
+system("PAUSE");
+return 0;	
+}
